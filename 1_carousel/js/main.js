451a7160708979317da3a401ef9d5659ae691724
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-01 12:28:14 +0100

    update main.js file

--- 1_carousel/js/main.js
+++ 1_carousel/js/main.js
@@ -3,23 +3,13 @@ let btnNext = document.getElementById('btn-next');
 let btnPrevious = document.getElementById('btn-previous');
 let i = 0;
 let currentImg = carousel[i];
-// console.log('out loop for:', currentImg);
 
 function iterationImg () {
   for (const iterator of carousel) {
     iterator.style.display = 'none';
     currentImg.style.display = 'block';
-    // console.log('iter:' ,iterator)
   }
 }
-
-// function iterationImg () {
-//   for (i; i < carousel.length; i++) {
-//     carousel[i].style.display = 'none';
-//     currentImg[i].style.display = 'block';
-//     console.log('in loop for:', carousel[i]);
-//   }
-// }
 
 iterationImg()
 
@@ -31,16 +21,11 @@ function next () {
     i = 0;
   }
   iterationImg()
-  console.log('img next:',i ,currentImg)
 }
 
 function previous () {
   currentImg = carousel[i];
   i--;
-  // if (i <= carousel.length) {
-  //   i = 0;
-  //   console.log('img previous:',i ,currentImg)
-  // }
   iterationImg()
 }
 
