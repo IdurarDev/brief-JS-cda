f8e4d7cbe3a95c058680769134b141352962171c
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-01 12:16:37 +0100

    improvement next function in main.js

--- 1_carousel/js/main.js
+++ 1_carousel/js/main.js
@@ -1,41 +1,48 @@
 let carousel = document.querySelectorAll('.imgCarousel');
-let btnPrevious = document.getElementById('btn-previous');
 let btnNext = document.getElementById('btn-next');
+let btnPrevious = document.getElementById('btn-previous');
 let i = 0;
 let currentImg = carousel[i];
-console.log('out loop for:', currentImg);
+// console.log('out loop for:', currentImg);
+
+function iterationImg () {
+  for (const iterator of carousel) {
+    iterator.style.display = 'none';
+    currentImg.style.display = 'block';
+    // console.log('iter:' ,iterator)
+  }
+}
 
 // function iterationImg () {
-//   for (const iterator of carousel) {
-//     iterator.style.display = 'none';
-//     currentImg.style.display = 'block';
+//   for (i; i < carousel.length; i++) {
+//     carousel[i].style.display = 'none';
+//     currentImg[i].style.display = 'block';
+//     console.log('in loop for:', carousel[i]);
 //   }
 // }
 
-// iterationImg()
-
-function iterationImg () {
-  for (i; i < carousel.length; i++) {
-    carousel[i].style.display = 'none';
-    currentImg[i].style.display = 'block';
-    console.log('in loop for:', carousel[i]);
-  }
-}
 iterationImg()
 
 function next () {
-  i++
   currentImg = carousel[i];
+  i++
+  console.log('currentImg:',i , currentImg)
+  if (i >= carousel.length) {
+    i = 0;
+  }
   iterationImg()
+  console.log('img next:',i ,currentImg)
 }
 
 function previous () {
-  i--;
   currentImg = carousel[i];
+  i--;
+  // if (i <= carousel.length) {
+  //   i = 0;
+  //   console.log('img previous:',i ,currentImg)
+  // }
   iterationImg()
 }
 
-
 btnNext.addEventListener('click', next);
 btnPrevious.addEventListener('click', previous);
-
