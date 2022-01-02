75a7cec7dee6e0dc491b866ef0cead35680fc692
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-02 20:26:13 +0100

    add function checkingScore

--- 4_monster-slayer/js/main.js
+++ 4_monster-slayer/js/main.js
@@ -18,20 +18,26 @@ percentM.innerHTML = pvM;
 console.log('percentM:', percentM)
 
 let attackMonster = Math.floor(Math.random() * ((10 - 5) + 1) + 5);
-let attackMy = Math.floor(Math.random() * ((10 - 3) + 1) + 3);;
+let attackMy = Math.floor(Math.random() * ((10 - 3) + 1) + 3);
 let specialMy = Math.floor(Math.random() * ((20 - 10) + 1) + 10);
 
 
-function hideBtnAttack () {
+function hideBtnAttacks () {
   btnStart.hidden = false;
   btnAttack.hidden = true;
 }
 
-hideBtnAttack()
+hideBtnAttacks()
 
 function starting () {
   btnStart.hidden = true;
   btnAttack.hidden = false;
+  my.value = 100;
+  monster.value = 100;
+  pvY = 100;
+  pvM = 100;
+  percentY.innerHTML = pvY;
+  percentM.innerHTML = pvM;
 }
 
 btnStart.addEventListener('click', starting)
@@ -45,6 +51,9 @@ function attack () {
   pvM -= attackMy;
   percentM.innerHTML = pvM;
   console.log('percentY:', percentY)
+
+  checkingScore()
+
 }
 
 punch.addEventListener('click', attack)
@@ -58,6 +67,9 @@ function specialAttack () {
   pvM -= specialMy;
   percentM.innerHTML = pvM;
   console.log('percentY:', percentY)
+
+  checkingScore()
+
 }
 
 specialPunch.addEventListener('click', specialAttack)
@@ -78,21 +90,44 @@ function heal () {
   percentM.innerHTML = pvM;
   monster.value = pvM
   
+  checkingScore()
 }
 
 health.addEventListener('click', heal)
 
 function giveUp () {
+  if(confirm('You are give up, new game ?')) {
+    hideBtnAttacks()
+  }
   my.value = 100;
   monster.value = 100;
   pvY = 100;
   pvM = 100;
   percentY.innerHTML = pvY;
   percentM.innerHTML = pvM;
-  hideBtnAttack()
 }
 
 stopping.addEventListener('click', giveUp)
+
+function checkingScore () {
+  if (pvY <= 0) {
+    if(confirm('Monster is winner, new game?')) {
+      starting()
+      // giveUp()
+    } else {
+      giveUp()
+      hideBtnAttacks()
+    }
+  } else if (pvM <= 0) {
+    if(confirm('You is winner, new game?')) {
+      starting()
+      // giveUp()
+    } else {
+      giveUp()
+      hideBtnAttacks()
+    }
+  }
+}
 
 // if (my.value > monster.value) {
 //   alert(`${you} is Winner`)
