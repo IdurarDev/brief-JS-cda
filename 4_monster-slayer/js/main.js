15ef1fa6e1f1c66333905cb3957389b93026477f
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-02 19:42:48 +0100

    add percentage in progress bar

--- 4_monster-slayer/js/main.js
+++ 4_monster-slayer/js/main.js
@@ -6,13 +6,21 @@ let punch = document.querySelector('.punch');
 let specialPunch = document.querySelector('.specialPunch');
 let health = document.querySelector('.health');
 let stopping = document.querySelector('.stop');
+let percentY = document.querySelector('.percentYou');
+let percentM = document.querySelector('.percentMonster');
 
-let attackMonster = 10;
-let specialMonster = 5;
-let attackMy = 5;
-let specialMy = 15;
-let healMy = 10;
-let damageMonster = 6;
+let pvY = 100;
+let pvM = 100;
+
+percentY.innerHTML = pvY;
+percentM.innerHTML = pvM;
+
+console.log('percentM:', percentM)
+
+let attackMonster = Math.floor(Math.random() * ((10 - 5) + 1) + 5);
+let attackMy = Math.floor(Math.random() * ((10 - 3) + 1) + 3);;
+let specialMy = Math.floor(Math.random() * ((20 - 10) + 1) + 10);
+
 
 function hideBtnAttack () {
   btnStart.hidden = false;
@@ -27,24 +35,49 @@ function starting () {
 }
 
 btnStart.addEventListener('click', starting)
+
 function attack () {
   my.value -= attackMonster;
+  pvY -= attackMonster;
+  percentY.innerHTML = pvY;
+  console.log('percentM:', percentM)
   monster.value -= attackMy;
+  pvM -= attackMy;
+  percentM.innerHTML = pvM;
+  console.log('percentY:', percentY)
 }
 
 punch.addEventListener('click', attack)
 
 function specialAttack () {
-  my.value -= specialMonster;
+  my.value -= attackMonster;
+  pvY -= attackMonster;
+  percentY.innerHTML = pvY;
+  console.log('percentM:', percentM)
   monster.value -= specialMy;
+  pvM -= specialMy;
+  percentM.innerHTML = pvM;
+  console.log('percentY:', percentY)
 }
 
 specialPunch.addEventListener('click', specialAttack)
 
-
 function heal () {
-  my.value += healMy
-  my.value -= damageMonster
+  if ((pvY += 10) >= 100) {
+    pvY = 100;
+  } else {
+    pvY += 10;
+  }
+  pvY -= attackMonster;
+  pvM;
+  console.log('attackMonster:', attackMonster)
+  console.log('pvY:', pvY)
+  percentY.innerHTML = pvY;
+  my.value = pvY;
+
+  percentM.innerHTML = pvM;
+  monster.value = pvM
+  
 }
 
 health.addEventListener('click', heal)
@@ -52,8 +85,17 @@ health.addEventListener('click', heal)
 function giveUp () {
   my.value = 100;
   monster.value = 100;
+  pvY = 100;
+  pvM = 100;
+  percentY.innerHTML = pvY;
+  percentM.innerHTML = pvM;
   hideBtnAttack()
 }
 
 stopping.addEventListener('click', giveUp)
 
+// if (my.value > monster.value) {
+//   alert(`${you} is Winner`)
+// } else {
+//   alert(`${monster} is Winner`)
+// }
